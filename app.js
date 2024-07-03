490e63956b1703af72dc9894b4f53f7495483ea4
Jitesh Sonigara <108194625+jiteshub@users.noreply.github.com>
2024-07-03 15:46:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.use(cors({
-    origin:"http://localhost:3000/",  
+    origin:"http://localhost:3000",  
 }))
 
 mongoose.connect(process.env.mongoconnect)
